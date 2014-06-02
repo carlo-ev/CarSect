9e0fcf320da883b00f537cf4197d8f527a1f89ba
carlo_ev <carlo_ev@unitec.edu>
2014-06-02 00:12:29 -0600

    changed to mongohq prod db

--- config/initializers/03_mongoose.js
+++ config/initializers/03_mongoose.js
@@ -5,7 +5,7 @@ module.exports = function(){
 		case 'development':
 			mongoose.connect('mongodb://localhost/dev');
 		case 'production':
-			mongoose.connect('mongodb://localhost/carsek');
+			mongoose.connect('mongodb://carlo_ev@unitec.edu:semperfi12792@kahana.mongohq.com:10023/carsek');
     }
     mongooseTypes = require('mongoose-3x-types');
     mongooseTypes.loadTypes(mongoose);
